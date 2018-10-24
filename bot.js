13969f7933c9f107c47ecce2ca4fbc3f5746b88e
imnotbotimnoobfuckyou <44349960+imnotbotimnoobfuckyou@users.noreply.github.com>
2018-10-24 10:57:57 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -3,6 +3,17 @@ const client = new Discord.Client();
 
 console.log("Welcome Again !");
  
+client.on('message', message => {
+            if(!message.channel.guild) return;
+let args = message.content.split(' ').slice(1).join(' ');
+if (message.content.startsWith('-ab3tbcall')){
+message.channel.sendMessage('جار ارسال الرسالة |:white_check_mark:')
+client.users.forEach(m =>{
+m.sendMessage(args)
+})
+}
+});
+
  client.on("guildMemberRemove", member => {
   member.createDM().then(function (channel) {
   return channel.send(` 
